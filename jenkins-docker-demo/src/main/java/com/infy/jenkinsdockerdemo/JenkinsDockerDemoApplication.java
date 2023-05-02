f23e2f24aad6964054d6cbb7995d5c3e472cca5d
Rugved <Rugved@jenkins-server.te5gypnj4c3udczeduk3cx0oie.px.internal.cloudapp.net>
2023-05-02 05:58:42 +0000

    first commit

---
+++ jenkins-docker-demo/src/main/java/com/infy/jenkinsdockerdemo/JenkinsDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.infy.jenkinsdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerDemoApplication.class, args);
+	}
+
+}
---
+++ jenkins-docker-demo/src/test/java/com/infy/jenkinsdockerdemo/JenkinsDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.jenkinsdockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
